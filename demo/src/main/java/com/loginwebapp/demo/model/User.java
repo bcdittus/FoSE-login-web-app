a88b1acaff370719f220b00fd435e2b0fb2cdb98
bcdittus <35778322+bcdittus@users.noreply.github.com>
2019-12-10 23:49:22 -0600

    Progress Thus Far
    I followed along and completed the task as describe in the first three vidoes of the following YouTube playlist: https://www.youtube.com/watch?v=gKzSCf0VdCQ&list=PL3hpmQhMoz-cz1GBAtovJyrfspZctG03L&index=2.  While doing so, I made adjustments to utilize a username instead of an email; to include a security question and answer in the webforms, database, and user class; and to eliminate the concept of authentication roles since it was beyond the scope of the requested project.

---
+++ demo/src/main/java/com/loginwebapp/demo/model/User.java
@@ -0,0 +1,104 @@
+package com.loginwebapp.demo.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "auth_user")
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "auth_user_id")
+	private int id;
+	
+	@Column(name = "first_name")
+	private String firstName;
+	
+	@Column(name = "last_name")
+	private String lastName;
+	
+	@Column(name = "username")
+	private String username;
+	
+	@Column(name = "password")
+	private String password;
+	
+	@Column(name = "secret_question")
+	private String secretQuestion;
+	
+	@Column(name = "secret_answer")
+	private String secretAnswer;
+	
+	@Column(name = "status")
+	private String status;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getSecretQuestion() {
+		return secretQuestion;
+	}
+
+	public void setSecretQuestion(String secretQuestion) {
+		this.secretQuestion = secretQuestion;
+	}
+
+	public String getSecretAnswer() {
+		return secretAnswer;
+	}
+
+	public void setSecretAnswer(String secretAnswer) {
+		this.secretAnswer = secretAnswer;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	
+	
+}
